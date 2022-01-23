8534bb06db44d07eb59cb77b2227e5ab08242600
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-01-23 21:25:14 +0100

    mfe1 add version name to app component

--- mfe1/src/app/app.component.ts
+++ mfe1/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, VERSION } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'mfe1';
+  public title = 'mfe1';
+  public readonly version = VERSION.major;
 }
