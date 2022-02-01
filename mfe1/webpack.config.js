9437222d9a353948b9351ed3ed4b4924091b1a0f
Vugar <vugar_abdullayev@outlook.com>
2022-02-01 22:56:03 +0100

    Feature/upgrade shell and mfe1 angular (#2)
    * Upgrade Shell and MFE1 angular and material to v13.2.0
    Co-authored-by: Vugar_Abdullayev <vugar_abdullayev@epam.com>

--- mfe1/webpack.config.js
+++ mfe1/webpack.config.js
@@ -21,18 +21,18 @@ module.exports = {
         },
         shared: {
           // Angular
-          "@angular/core": {requiredVersion: '13.1.3' },
-          "@angular/common": {requiredVersion: '13.1.3' },
-          "@angular/common/http": {requiredVersion: '13.1.3' },
-          "@angular/router": {requiredVersion: '13.1.3' },
-          "@angular/platform-browser/animations": {requiredVersion: '13.1.3' },
+          "@angular/core": {requiredVersion: '13.2.0' },
+          "@angular/common": {requiredVersion: '13.2.0' },
+          "@angular/common/http": {requiredVersion: '13.2.0' },
+          "@angular/router": {requiredVersion: '13.2.0' },
+          "@angular/platform-browser/animations": {requiredVersion: '13.2.0' },
           // RxJs
            "rxjs": { requiredVersion: '7.4.0' },
            "rxjs/operators": { requiredVersion: '7.4.0' },
            // Material
-          "@angular/cdk": {requiredVersion: '13.1.3' },
-          "@angular/material/table": {requiredVersion: '13.1.3' },
-          "@angular/material/button": {requiredVersion: '13.1.3' },
+          "@angular/cdk": {requiredVersion: '13.2.0' },
+          "@angular/material/table": {requiredVersion: '13.2.0' },
+          "@angular/material/button": {requiredVersion: '13.2.0' },
     },
   })
   ],
