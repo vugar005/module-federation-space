346c3fe466c120c88612b246d05e18f791e955c6
Vugar_Abdullayev <vugar_abdullayev@epam.com>
2022-02-02 14:11:21 +0100

    feature/connect-mfe2

--- shell/src/app/app-routing.module.ts
+++ shell/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MainComponent } from './main/main.component';
 import { Mfe1WrapperComponent } from './mfe1-wrapper/mfe1-wrapper.component';
 import { startsWith } from './router.utils';
+import { WrapperComponent } from './wrapper/wrapper.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'main', pathMatch: 'full' },
   { path: 'main', component: MainComponent },
   { matcher: startsWith('mfe1'), component: Mfe1WrapperComponent, data: { importName: 'mfe1', elementName: 'mfe1-element' } },
+  { matcher: startsWith('mfe2'), component: WrapperComponent, data: { importName: 'mfe2', elementName: 'mfe2-element' } },
 ];
 
 @NgModule({
